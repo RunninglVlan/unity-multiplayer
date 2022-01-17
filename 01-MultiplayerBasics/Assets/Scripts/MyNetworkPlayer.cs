799aa7045d262d9596a70919a992d29a9ebf9ae3
RunninglVlan <RedCapShortCut@gmail.com>
2022-01-17 21:18:19 +0200

    feat: completed 7. SyncVar Callbacks

--- 01-MultiplayerBasics/Assets/Scripts/MyNetworkPlayer.cs
+++ 01-MultiplayerBasics/Assets/Scripts/MyNetworkPlayer.cs
@@ -1,13 +1,36 @@
 ﻿using Mirror;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MyNetworkPlayer : NetworkBehaviour {
-    [SyncVar, SerializeField] private string displayName = "Missing Name";
-    [SyncVar, SerializeField] private Color displayColor = Color.magenta;
+    [SyncVar(hook = nameof(SetTitle)), SerializeField]
+    string displayName = "Missing Name";
+
+    [SyncVar(hook = nameof(SetRendererColor)), SerializeField]
+    Color displayColor = Color.magenta;
+
+    [SerializeField] Transform titlePoint;
+    [SerializeField] Renderer colorRenderer;
+
+    Label title;
+    Camera mainCamera;
 
     [Server]
     public void SetDisplayName(string name) => displayName = name;
 
     [Server]
     public void SetDisplayColor(Color value) => displayColor = value;
+
+    void SetTitle(string _, string newValue) => title.text = newValue;
+    void SetRendererColor(Color _, Color newValue) => colorRenderer.material.color = newValue;
+
+    void Awake() {
+        title = GetComponent<UIDocument>().rootVisualElement.Q<Label>();
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate() {
+        var position = RuntimePanelUtils.CameraTransformWorldToPanel(title.panel, titlePoint.position, mainCamera);
+        title.parent.transform.position = position;
+    }
 }
--- 01-MultiplayerBasics/README.md
+++ 01-MultiplayerBasics/README.md
@@ -6,4 +6,5 @@ Multiplayer Basics, 11 Lectures: [1](https://www.udemy.com/course/unity-multipla
 - [NetworkManager](https://mirror-networking.gitbook.io/docs/components/network-manager) methods: `OnClient...` is called on the client, `OnServer...` is called on the server
 - [NetworkBehaviour](https://mirror-networking.gitbook.io/docs/components/networkbehaviour) relies on `NetworkIdentity` (which is placed on the Player) and can have SyncVars
   - [SyncVars](https://mirror-networking.gitbook.io/docs/guides/synchronization/syncvars) are properties of classes that inherit from NetworkBehaviour, which are synchronized from the server to all clients automatically
+  - SyncVars have [Hooks](https://mirror-networking.gitbook.io/docs/guides/synchronization/syncvar-hooks) - methods that are triggered on the client when its SyncVar is changed
   - The supported sync [Data types](https://mirror-networking.gitbook.io/docs/guides/data-types)
